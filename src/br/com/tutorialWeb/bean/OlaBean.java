3d1983993b20cbae0df45b56e95ac9940f1edb22
alice.leite <alice.leite@RJOD212084>
2017-10-30 11:48:39 -0200

    commit inicial

---
+++ src/br/com/tutorialWeb/bean/OlaBean.java
@@ -0,0 +1,20 @@
+package br.com.tutorialWeb.bean;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class OlaBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
